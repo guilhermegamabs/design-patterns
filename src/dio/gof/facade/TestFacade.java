50b57e6c9e87aa2df2c8c0d40d76ca3acf03c3f6
Guilherme Gama <guilhermegamabs@gmail.com>
2024-07-10 15:25:34 -0300

    Facade com Java Puro

---
+++ src/dio/gof/facade/TestFacade.java
@@ -0,0 +1,8 @@
+package dio.gof.facade;
+
+public class TestFacade {
+    public static void main(String[] args) {
+        Facade facade = new Facade();
+        facade.migrarCliente("Guilherme", "11111111");
+    }
+}
